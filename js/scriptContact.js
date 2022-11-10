b8718a959d16812202ffe5faa5e275c2e91d90ca
Mateus Rocha <mateus_lr@outlook.com>
2022-11-09 22:43:10 -0300

    improve portfolio

---
+++ js/scriptContact.js
@@ -0,0 +1,19 @@
+var subtitle = document.getElementById('subtitle');
+
+var typewriter = new Typewriter(subtitle, {
+  loop: true,
+  delay: 110
+});
+
+typewriter.typeString("<strong>Avaliable for ...<strong>")
+  .pauseFor(1200)
+  .deleteChars(3)
+  .typeString("<strong>questions<strong>")
+  .pauseFor(1200)
+  .deleteChars(9)
+  .typeString("<strong>jobs<strong>")
+  .pauseFor(1200)
+  .deleteChars(4)
+  .typeString("<strong>and coffe ☕<strong>")
+  .pauseFor(2000)
+  .start();
