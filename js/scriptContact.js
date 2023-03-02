df1a7782cae02e446854d785b50a01c3012ff318
Mateus Rocha <mateus_lr@outlook.com>
2023-03-02 00:05:26 -0300

    style changes

--- js/scriptContact.js
+++ js/scriptContact.js
@@ -2,20 +2,21 @@ var subtitle = document.getElementById('subtitle');
 
 var typewriter = new Typewriter(subtitle, {
   loop: true,
-  delay: 95
+  delay: 140
 });
 
 typewriter.typeString("<strong>Available for ...<strong>")
-  .pauseFor(1000)
+  .pauseFor(1500)
   .deleteChars(3)
   .typeString("<strong>questions<strong>")
-  .pauseFor(1000)
+  .pauseFor(1500)
   .deleteChars(9)
   .typeString("<strong>jobs<strong>")
-  .pauseFor(1000)
+  .pauseFor(1500)
   .deleteChars(4)
   .typeString("<strong>coffe ☕<strong>")
   .pauseFor(2000)
+  .pauseFor(5000)
   .start();
 
   // AOS Animation
