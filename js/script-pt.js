9871590bc06c3fca63171cf80f6e4ded93b9e6bd
Mateus Rocha <mateus_lr@outlook.com>
2023-03-04 03:07:25 -0300

    addition of the English version of the website

---
+++ js/script-pt.js
@@ -0,0 +1,59 @@
+const btnMobile = document.getElementById('btnMobile');
+const divSite = document.getElementById('site');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  divSite.classList.toggle('active');
+  btnMobile.classList.toggle('is-active');
+}
+
+function closeMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.remove('active');
+  divSite.classList.remove('active');
+  btnMobile.classList.remove('is-active');
+}
+
+function closeMenuMobile(event) {
+  if(!event.target.closest('#nav')) {
+    closeMenu();
+  }
+}
+
+document.addEventListener('click', closeMenuMobile);
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+const handleScroll = () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+// TYPEWRITER EFFECT
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 100
+});
+
+typewriter.typeString("<strong>Olá, <strong>")
+  .pauseFor(1000)
+  .typeString("<strong>me chamo Mateus<strong>")
+  .pauseFor(5000)
+  .start();
+
+// AOS ANIMATION
+
+AOS.init(
+  {
+    duration: 1200,
+    once: true,
+  }
+);
