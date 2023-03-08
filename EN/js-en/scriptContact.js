1c1a8cc083a5c9ac542e4d5dac7732ff025ad95f
Mateus Rocha <mateus_lr@outlook.com>
2023-03-08 00:29:38 -0300

    style changes

--- EN/js-en/scriptContact.js
+++ EN/js-en/scriptContact.js
@@ -1,23 +1,23 @@
-var subtitle = document.getElementById('subtitle');
+// var subtitle = document.getElementById('subtitle');
 
-var typewriter = new Typewriter(subtitle, {
-  loop: true,
-  delay: 140
-});
+// var typewriter = new Typewriter(subtitle, {
+//   loop: true,
+//   delay: 140
+// });
 
-typewriter.typeString("<strong>Available for ...<strong>")
-  .pauseFor(1500)
-  .deleteChars(3)
-  .typeString("<strong>questions<strong>")
-  .pauseFor(1500)
-  .deleteChars(9)
-  .typeString("<strong>jobs<strong>")
-  .pauseFor(1500)
-  .deleteChars(4)
-  .typeString("<strong>coffe ☕<strong>")
-  .pauseFor(2000)
-  .pauseFor(5000)
-  .start();
+// typewriter.typeString("<strong>Available for ...<strong>")
+//   .pauseFor(1500)
+//   .deleteChars(3)
+//   .typeString("<strong>questions<strong>")
+//   .pauseFor(1500)
+//   .deleteChars(9)
+//   .typeString("<strong>jobs<strong>")
+//   .pauseFor(1500)
+//   .deleteChars(4)
+//   .typeString("<strong>coffe ☕<strong>")
+//   .pauseFor(2000)
+//   .pauseFor(5000)
+//   .start();
 
   // AOS Animation
   AOS.init(
--- js/scriptContact-pt.js
+++ js/scriptContact-pt.js
@@ -1,23 +1,23 @@
-var subtitle = document.getElementById('subtitle');
+// var subtitle = document.getElementById('subtitle');
 
-var typewriter = new Typewriter(subtitle, {
-  loop: true,
-  delay: 140
-});
+// var typewriter = new Typewriter(subtitle, {
+//   loop: true,
+//   delay: 140
+// });
 
-typewriter.typeString("<strong>Disponível para ...<strong>")
-  .pauseFor(1500)
-  .deleteChars(3)
-  .typeString("<strong>perguntas<strong>")
-  .pauseFor(1500)
-  .deleteChars(9)
-  .typeString("<strong>trabalhos<strong>")
-  .pauseFor(1500)
-  .deleteChars(9)
-  .typeString("<strong>tomar um café ☕<strong>")
-  .pauseFor(2000)
-  .pauseFor(5000)
-  .start();
+// typewriter.typeString("<strong>Disponível para ...<strong>")
+//   .pauseFor(1500)
+//   .deleteChars(3)
+//   .typeString("<strong>perguntas<strong>")
+//   .pauseFor(1500)
+//   .deleteChars(9)
+//   .typeString("<strong>trabalhos<strong>")
+//   .pauseFor(1500)
+//   .deleteChars(9)
+//   .typeString("<strong>tomar um café ☕<strong>")
+//   .pauseFor(2000)
+//   .pauseFor(5000)
+//   .start();
 
   // AOS Animation
   AOS.init(
